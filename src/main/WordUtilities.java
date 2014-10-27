f4ef4a52a7a43b480a0e3e0b2a8c5c97058fb257
Alan Ly <me@alanly.ca>
2014-10-26 20:45:52 -0400

    Clean up code; sort words.

--- src/main/WordUtilities.java
+++ src/main/WordUtilities.java
@@ -4,79 +4,107 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.*;
 
+/**
+ * @author Thomas Rahn
+ * @author Alan Ly
+ */
 public class WordUtilities {
 	
 	private static int totalSpam = 0;
 	private static int totalHam = 0;
 	
-	private static String delimeter = "   ";
+	private static String DELIMETER = "   ";
 	
 	/**
 	 * This method accepts a list of words and will save them into a file with their corresponding values. (Frequency and probability)
 	 * 
-	 * @param List<Word>	List of words to be saved into a file
-	 * 
+	 * @param words	     the list of words to be saved into a file
+	 * @param outputPath the destination of the output file.
 	 */
-	public static void SaveWords(List<Word> words){
+	public static void SaveWords(List<Word> words, String outputPath){
+		LocalTime processStartTime = LocalTime.now();
+
 		try {
-			 
-			System.out.println("starting");
-			String content = generateWordString(words);
-			System.out.println("ending");
- 
-			File file = new File("words/model.txt");
- 
-			// if file doesnt exists, then create it
-			if (!file.exists()) {
-				file.createNewFile();
+			// Sort our incoming list of words.
+			System.out.print("Sort words...");
+			Collections.sort(words);
+			System.out.println("Done.");
+
+			// Generate a string listing of our words.
+			String output = generateWordString(words);
+
+			File outputFile = new File(outputPath);
+			System.out.println("Using output file \"" + outputFile.getAbsolutePath() + "\".");
+
+			// Create a non-existing file.
+			if (! outputFile.exists()) {
+				System.out.print("File does not exist; creating...");
+				outputFile.createNewFile();
+				System.out.println("Done.");
 			}
- 
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+
+			// Create the writer instances,
+			FileWriter fw = new FileWriter(outputFile.getAbsoluteFile());
 			BufferedWriter bw = new BufferedWriter(fw);
-			bw.write(content);
+
+			System.out.print("Writing output file...");
+			bw.write(output);
 			bw.close();
- 
-			System.out.println("Done");
- 
-		} catch (IOException e) {
-			e.printStackTrace();
+			System.out.println("Done.");
+
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
+
+		LocalTime processEndTime = LocalTime.now();
+		Duration processDuration = Duration.between(processStartTime, processEndTime);
+
+		System.out.println("File Output Duration: "+processDuration.getSeconds()+" sec.");
 	}
 	
 	/**
 	 * This method will accept a List of words and generate a string.
 	 * 
-	 * @param List<Word> A list of words to be converted to a string
+	 * @param words A list of words to be converted to a string
 	 * 
 	 * @return String the string generated from a list of words
 	 */
-	private static String generateWordString(List<Word> words){
-		String content = "";
-		Iterator<Word> it = words.iterator();
-		int counter = 1;
-		while(it.hasNext()){
-			Word w = it.next();
-			
-			double spamSmooth = w.getSpamSmooth() / (totalSpam + (0.5 * words.size()));
-			double hamSmooth = w.getHamSmooth() / (totalHam + (0.5 * words.size()));
-			
-			content += counter + 
-					delimeter + w.getWord() + 
-					delimeter + w.getHamFrequency() + 
-					delimeter + hamSmooth + 
-					delimeter + w.getSpamFrequency() + 
-					delimeter + spamSmooth + "\n";
-			
-			counter++;
+	private static String generateWordString(List<Word> words) {
+		String output = "";
+		int wordCount = words.size();
+		int index = 0;
+		int progress = 0;
+
+		System.out.print("Generating output.");
+
+		for (Word word : words) {
+			// Determine the smoothed values.
+			double spamSmooth = word.getSpamSmooth() / (totalSpam + (0.5 * words.size()));
+			double hamSmooth = word.getHamSmooth() / (totalHam + (0.5 * words.size()));
+
+			// Create our output string
+			output += ++index + DELIMETER;
+			output += word.getWord() + DELIMETER;
+			output += word.getHamFrequency() + DELIMETER;
+			output += hamSmooth + DELIMETER;
+			output += word.getSpamFrequency() + DELIMETER;
+			output += spamSmooth + "\n";
+
+			double currentProgress = ((double)index / wordCount) * 100;
+			if ((currentProgress - progress) >= 1.0)
+			{
+				System.out.print(".");
+			}
+			progress = (int) currentProgress;
 		}
-		
-		return content;
-		
+
+		System.out.println("Done.");
+
+		return output;
 	}
 	/**
 	 * Set the total number of spam words and total number of ham words
@@ -102,6 +130,7 @@ public class WordUtilities {
 	public static List<Word> CreateWordList(Map<String,Integer> spam, Map<String,Integer> ham)
 	{
 		List<Word> words = new ArrayList<Word>();
+
 		Iterator<String> it = spam.keySet().iterator();
 		while(it.hasNext()){
 			String element = it.next();
@@ -124,6 +153,5 @@ public class WordUtilities {
 		}
 		
 		return words;
-	
 	}
 }
